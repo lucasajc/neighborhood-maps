f25139f3b5ad4859e86ebe0fdffc9cb9eaf66597
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2018-07-14 15:59:35 -0300

    feat: search locations

--- src/App.js
+++ src/App.js
@@ -3,14 +3,57 @@ import { Route } from "react-router-dom";
 import './App.css';
 import SearchContainer from "./components/SearchContainer";
 import MapContainer from "./components/MapContainer";
+import {GoogleApiWrapper} from 'google-maps-react';
+import Geocode from "react-geocode";
+
+const apiKey = 'AIzaSyCNvqo698MfJGSTnA1aednek7qyKD_hwhs';
 
 class App extends Component {
 
   state = {
-    initialCenter:{
+    mapCenter:{
             lat: -19.932100,
             lng: -43.938072
-    }
+    },
+    searchResults: []
+  }
+
+  searchPlaces = query=>{
+
+    this.setState({ searchResults: [] });
+
+    // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+    Geocode.setApiKey(apiKey);
+
+    console.log(query);
+    // Make sure the address isn't blank.
+        if (query === '') {
+          console.log('You must enter an area, or address.');
+        } else {
+          // Get latidude & longitude from address.
+          Geocode.fromAddress(query).then(
+            response => {
+              response.results.map(item=>{
+                  // Get address from latidude & longitude.
+                  const { lat, lng } = item.geometry.location;
+                  Geocode.fromLatLng(lat, lng).then(
+                  response => {
+                    this.setState({ searchResults: response.results});
+                    console.log(this.state.searchResults);
+                  },
+                  error => {
+                    console.error(error);
+                  }
+                );
+              });
+            },
+            error => {
+              //TODO tratar zero results
+              console.error(error);
+            }
+          );
+
+        }
   }
 
   render() {
@@ -21,12 +64,16 @@ class App extends Component {
             exact path="/"
             render={() => (
               <div>
+                <div className="searchScroll"></div>
                 <div className="searchBox">
-                  <SearchContainer/>
+                  <SearchContainer
+                    onSearch={this.searchPlaces}
+                    locations={this.state.searchResults}
+                  />
                 </div>
                 <div className="mapBox">
                    <MapContainer
-                    center={this.state.initialCenter}
+                    center={this.state.mapCenter}
                     />
                 </div>
                  
@@ -41,4 +88,6 @@ class App extends Component {
   }
 }
 
-export default App
+export default GoogleApiWrapper({
+  apiKey: (apiKey)
+})(App)
--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -3,13 +3,44 @@ import '../App.css';
 
 class SearchContainer extends Component {
 
-  render() {
+  handleSearch = e =>{
+    e.preventDefault();
+    this.props.onSearch(e.target.value);
+  } 
 
+  render() {
+    const {locations} = this.props;
+    locations===undefined? console.log('') : console.log(locations);
     return (
-                <div>
-                </div>
+
+      <div className="search-places">
+        <div className="search-places-bar">
+              <div className="search-places-input-wrapper">
+                <input
+                  type="text"
+                  onChange={this.handleSearch}
+                  placeholder="Search by area or adress"
+                />
+                 
+
+              </div>
+              
+        </div>
+        <div className="locations-list-container">
+                <ol className="locations-list">
+
+                      {locations===undefined? '': locations.map(location => (
+                        <li key={location.place_id}>
+                          <p>{location.formatted_address}</p>
+                        </li>
+                      ))}                  
+                </ol>
+              </div>
+              
+      </div>
     );
   }
+
 }
 
 export default SearchContainer
